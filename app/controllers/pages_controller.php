24a61b49718ed8b074ac20fe3d57b944094b7fd3
Luis Jhonne <luis.jhonne@institutoazimuth.com.br>
2014-11-20 18:17:06 -0300

    Falta terminar o controle das rotas (get notes)

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -9,8 +9,17 @@ class PagesController extends BaseController {
     }
 
     protected function _notes(){
-    	$this->page_title = 'Experimente';
-    	$this->render("pages/notes");
+    	
+    	if ( isset( $this->params['id']) ){
+    		
+    		$result = $this->__buscar( $this->params ['id']);
+    		
+    		if ( !$result ){ //Senão localizar nenhuma sala com o nome requisitado, redireciona para o index
+    			$this->render("pages/index");
+    		} else {
+    			$this->render("pages/notes");
+    		}
+    	}
     	
     }
     protected function _show(){
@@ -31,14 +40,13 @@ class PagesController extends BaseController {
     protected function _salva() {
     	if ( isset ($this->request_params['text']) && isset( $this->params['id']) ){
     		
-    		$conn = Connection::getConnection();
-    		$conn->setQuery( "SELECT * FROM tb_mensagem WHERE cl_codigo_sala = '". $this->params['id']."' " );
-    		$result = $conn->execut_query('simplex');
+	   		$result = $this->__buscar( $this->params['id']);
     		
-    		$dados['cl_codigo_mensagem'] = '1';
+    		$dados['cl_codigo_mensagem'] = $result['cl_codigo_mensagem'];
     		$dados['cl_codigo_sala'] = $this->params['id'];
     		$dados['cl_mensagem'] =  $this->request_params['text'];
     		
+    		$conn = Connection::getConnection();
     		$conn->update($dados,"tb_mensagem","cl_codigo_mensagem", null, true); //Atualiza a mensagem no servidor
     		
     	}
@@ -48,11 +56,32 @@ class PagesController extends BaseController {
     	
     	if ( isset($this->request_params['sala'])){
     		
+    		//Busco a sala
+    		$result = $this->__buscar( $this->request_params['sala']);
+    		
+    		if ( !$result ){
+    			$this->__salvar();
+    		}
+    		
+    		return $this->JSONResponse( array ( 'novaUrl' => $this->request_params['sala'] ) );
+    		
     	}
+    }
+    
+    private function __buscar( $idSala ){
     	
+    	$conn = Connection::getConnection();
+    	$conn->setQuery( "SELECT * FROM tb_mensagem WHERE cl_codigo_sala = '".$idSala."' " );
+    	$result = $conn->execut_query('simplex');
+    	return $result;
+    }
+    private function __salvar(){
     	
-    	return $this->JSONResponse( array ( 'novaUrl' => '1') );
+    	$dados ['cl_codigo_sala'] = $this->request_params['sala'];
+    	$dados ['cl_mensagem'] = null;
     	
+    	$conn = Connection::getConnection();
+    	return $conn->insert( $dados, 'tb_mensagem', 'cl_codigo_mensagem', TRUE );
     }
     
 }
